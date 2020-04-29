5a10f66d343538543aca7a99468587c2c1c66d2c
JJ Mase <mase@usc.edu>
2020-04-28 20:59:21 -0700

    Prevented caching

--- website.py
+++ website.py
@@ -15,5 +15,14 @@ def hardwareInformation():
     return render_template('HardwareInformationInterface.html')
 
 
+@app.after_request
+def add_header(r):
+    r.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    r.headers['Pragma'] = 'no-cache'
+    r.headers['Expires'] = '0'
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)
