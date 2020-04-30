ce60b079a6c123f258b5ca5cfce59f9345b0fdd3
John (JJ) Mase <mase@usc.edu>
2020-04-30 04:47:37 -0700

    Fixed

--- animations/Strobe.py
+++ animations/Strobe.py
@@ -11,14 +11,10 @@ class Strobe(threading.Thread):
 
     def run(self):
         while self.shouldRun:
-            for i in range(self.numPixels):
-                self.pixels[i] = ((255,255,255))
+            self.pixels.fill((255,255,255))
             self.pixels.show()
-            time.sleep(0.05)
-            for i in range(self.numPixels):
-                self.pixels[i] = ((0,0,0))
+            self.pixels.fill((0,0,0))
             self.pixels.show()
-            time.sleep(0.05)
 
     def stop(self):
         self.shouldRun = False
