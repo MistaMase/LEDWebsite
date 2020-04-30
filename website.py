1dc6120838ab7e9a9e0c233de9c47c7eea7e8501
JJ Mase <mase@usc.edu>
2020-04-29 23:24:41 -0700

    SocketIO set up

--- website.py
+++ website.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_socketio import SocketIO, emit
+
+from gpiozero import CPUTemperature
 
 import socket
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'myspecialsecret'
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def redirecttohome():
@@ -16,6 +22,14 @@ def home():
 def manualinput():
     return render_template('ManualInputInterface.html', ipaddress=getIpAddress())
 
+@socketio.on('MI Connection')
+def manualConnected():
+    print("MI Connected")
+
+@socketio.on('MI Update Client')
+def manualColorChange(message):
+    print("Colors:" + message)
+
 @app.route('/codeinput')
 def codeinput():
     return render_template('CodeInputInterface.html', ipaddress=getIpAddress())
@@ -23,6 +37,17 @@ def codeinput():
 @app.route('/hardwareinformation')
 def hardwareinformation():
     return render_template('HardwareInformationInterface.html', ipaddress=getIpAddress())
+
+@socketio.on('HI Connection')
+def firstHIConnection():
+    print("HI Connection. Sending INFO")
+    emit('HI Update Server', { 'mode': 'test', 'fanSpeed': '50', 'micValue': '70', 'piTemp': getCPUTemp() })
+
+@socketio.on("HI Update Client")
+def sendHardwareInfo():
+    print("HI Requesting Periodic Update")
+    emit('HI Update Server', { 'mode': 'test', 'fanSpeed': '50', 'micValue': '70', 'piTemp': getCPUTemp() })
+
 
 # Prevent browsers from caching anything
 @app.after_request
@@ -39,6 +64,12 @@ def getIpAddress():
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
+# Gets the Pi's CPU temp to send to the client
+def getCPUTemp():
+    cpu = CPUTemperature()
+    return cpu.temperature
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    print(getCPUTemp())
+    socketio.run(app, host='0.0.0.0', port='5050', debug=True)
