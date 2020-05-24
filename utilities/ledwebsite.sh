70e7cb1bfc91f9bf99938bb1d7e40120ea9b5955
JJ Mase <mase@usc.edu>
2020-05-24 23:36:43 +0100

    Updated services

--- utilities/ledwebsite.sh
+++ utilities/ledwebsite.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ### BEGIN INIT INFO
 # Provides:		ledwebsite
-# Required-Start:	$remote_fs $syslog ledwebsite_update
-# Required-Stop:	$remote_fs $syslog ledwebsite_update
+# Required-Start:	$remote_fs $syslog $network ledwebsite_update
+# Required-Stop:	$remote_fs $syslog $network ledwebsite_update
 # Default-Start:	2 3 4 5
 # Default-Stop:		0 1 6
 # Short-Description:	Starts the LED Controller at boot
