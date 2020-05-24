0a25e0cbca3986b639e6175e20aa545154f47fcb
JJ Mase <mase@usc.edu>
2020-05-25 00:22:44 +0100

    Fixed update scripts

---
+++ utilities/ledwebsite.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:		ledwebsite
+# Required-Start:	$remote_fs $syslog $network ledwebsite_update
+# Required-Stop:	$remote_fs $syslog $network ledwebsite_update
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	Starts the LED Controller at boot
+# Description:		Starts the LED Controller at boot
+### END INIT INFO
+
+python3 home/pi/LEDWebsite/src/website.py
+
