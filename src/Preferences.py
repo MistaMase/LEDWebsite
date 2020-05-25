8533d12dfede7a5e2586ee34f7c7864583e1bc95
JJ <jjjmase123@gmail.com>
2020-05-25 12:13:03 -0700

    Type error

--- src/Preferences.py
+++ src/Preferences.py
@@ -134,7 +134,7 @@ class Preferences:
             # Update the file accordingly
             with open('/home/pi/LEDWebsite/preferences/debug.txt', 'w') as pref_file:
                 for key, value in self.debug_preferences.items():
-                    pref_file.write(key + ':' + value)
+                    pref_file.write(str(key) + ':' + str(value))
         except KeyError:
             if self.get_debug_preferences('preferences-debug'):
                 print('Error Updating Debug Preferences Dictionary with ' + str(key))
@@ -152,7 +152,7 @@ class Preferences:
             # Update the file accordingly
             with open('/home/pi/LEDWebsite/preferences/animation-order.txt', 'w') as pref_file:
                 for key, value in self.animation_preferences.items():
-                    pref_file.write(key + ':' + value)
+                    pref_file.write(str(key) + ':' + str(value))
         except KeyError:
             if self.get_debug_preferences('preferences-debug'):
                 print('Error Updating Animation Preferences Dictionary with ' + str(key))
@@ -168,7 +168,7 @@ class Preferences:
             # Update the file accordingly
             with open('/home/pi/LEDWebsite/preferences/custom-colors.txt', 'w') as pref_file:
                 for key, value in self.color_preferences.items():
-                    pref_file.write(key + ':' + value)
+                    pref_file.write(str(key) + ':' + str(value))
         except KeyError:
             if self.get_debug_preferences('preferences-debug'):
                 print('Error Updating Color Preferences Dictionary with ' + str(key))
@@ -185,7 +185,7 @@ class Preferences:
             # Update the file accordingly
             with open('/home/pi/LEDWebsite/preferences/setup.txt', 'w') as pref_file:
                 for key, value in self.setup_preferences.items():
-                    pref_file.write(key + ':' + value)
+                    pref_file.write(str(key) + ':' + str(value))
         except KeyError:
             if self.get_debug_preferences('preferences-debug'):
                 print('Error Updating Setup Preferences Dictionary with ' + str(key))
@@ -201,7 +201,7 @@ class Preferences:
             # Update the file accordingly
             with open('/home/pi/LEDWebsite/preferences/info.txt', 'w') as pref_file:
                 for key, value in self.info.items():
-                    pref_file.write(key + ':' + value)
+                    pref_file.write(str(key) + ':' + str(value))
         except KeyError:
             if self.get_debug_preferences('preferences-debug'):
                 print('Error Updating Info Dictionary with ' + str(key))
