7a421c8dbe26512c7769f2bd51cfb246f990d1ab
JJ <jjjmase123@gmail.com>
2020-05-25 11:08:40 -0700

    more testing

--- src/website.py
+++ src/website.py
@@ -80,7 +80,6 @@ def manualConnected():
     if preferences.get_debug_preferences('website-debug'):
         print("MI Connected")
     emit('MI Parameters', scenes.getAnimationOptions())
-    print(list(preferences.get_color_preferences().keys()))
     emit('MI Color Profiles', list(preferences.get_color_preferences().keys()))
 
 # Socketio response for Manual Interface webpage color change
