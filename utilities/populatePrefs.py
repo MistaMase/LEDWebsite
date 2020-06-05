285df9e6ae4a62e93de5560bcf16ac9ea9c2a3ed
JJ <jjjmase123@gmail.com>
2020-06-05 00:30:57 -0700

    testing

--- utilities/populatePrefs.py
+++ utilities/populatePrefs.py
@@ -9,17 +9,17 @@ with open('mandatory_setup.json', 'r') as file:
     mandatory_parameters = json.load(file)
 
 for file in mandatory_parameters:
-    print(str(file))
     print('/home/pi/LEDWebsite/preferences/' + str(file) + '.json')
     if path.exists('/home/pi/LEDWebsite/preferences/' + str(file) + '.json'):
         # Read the values in that file
         with open('/home/pi/LEDWebsite/preferences/' + str(file) + '.json', 'r') as pref_file:
             params = json.load(pref_file)
 
+        print(params)
+
         # Brute force check if the file has the key
         for key in mandatory_parameters[file]:
             print(key)
-            print(mandatory_parameters[file][key])
             #if mandatory_parameters[file][key] in params.keys():
             #    print(params[key])
 
