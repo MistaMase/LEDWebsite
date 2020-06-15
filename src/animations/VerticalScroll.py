818d722b66c1f94802844a8293c7c39f20df710c
John (JJ) Mase <mase@usc.edu>
2020-06-14 21:49:58 -0700

    Vertical Scroll Testing

---
+++ src/animations/VerticalScroll.py
@@ -0,0 +1,29 @@
+from BaseAnimation import BaseAnimation
+import time
+import letterlayout
+
+class VerticalScroll(BaseAnimation):
+    def __init__(self, pixels, numPixels):
+        super().__init__(pixels, numPixels, 'VerticalScroll')
+        self.shouldRun = True
+
+        # Internal settings for the possible options
+        self.parameters = {
+            'Sleep': 0.0
+        }
+
+        # Options returned by the website
+        self.options = [
+            ['Slider', 'Sleep', (0, 1, self.parameters['Sleep'])]
+        ]
+
+    def run(self):
+        while self.shouldRun:
+            for row in range(len(I)):
+                for col in range(len(I[0])) :
+                    self.pixels[row][col] = (255, 255, 255)
+                self.pixels.show()
+                time.sleep(float(self.parameters['Sleep']))
+
+    def stop(self):
+        self.shouldRun = False
